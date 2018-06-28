2886f3d4734ac62ac93191c3c915c7b41bae0ec6
Mitchell Durbin <dude0413@gmail.com>
2018-06-28 14:41:12 -0400

    Update zergEasyBot.py

--- zergEasyBot.py
+++ zergEasyBot.py
@@ -6,8 +6,23 @@ from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
 import random
 
 # Setup Game Variables #
-race = Race.Protoss
-enemy_race = Race.Terran
-enemy_difficulty = Difficulty.Hard
+race = Race.Zerg
+enemy_race = Race.Protoss
+enemy_difficulty = Difficulty.Easy
 realtime_condition = False
 map_name = "AbyssalReefLE"
+
+class MitchyBot(sc2.BotAI):
+ 
+    def __init__(self): 
+        self.ITERATIONS_PER_MINUTE = 165
+        self.MAX_WORKERS = 50
+     
+async def on_step(self, iteration):
+        self.iteration = iteration
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.expand()
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
+        await self.attack()
