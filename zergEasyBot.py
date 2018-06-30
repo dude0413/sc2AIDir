f4f3bf55a6b7a78172bf12ff2a7d5099328bac1e
Mitchell Durbin <dude0413@gmail.com>
2018-06-30 14:00:47 -0400

    Update zergEasyBot.py

--- zergEasyBot.py
+++ zergEasyBot.py
@@ -1,8 +1,7 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
- CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
+from sc2.constants import HATCHERY, LARVA, QUEEN, DRONE,
 import random
 
 # Setup Game Variables #
@@ -11,6 +10,7 @@ enemy_race = Race.Protoss
 enemy_difficulty = Difficulty.Easy
 realtime_condition = False
 map_name = "AbyssalReefLE"
+replay_name = "ZvTEasyFirst.SC2Replay"
 
 class MitchyBot(sc2.BotAI):
  
@@ -20,9 +20,19 @@ class MitchyBot(sc2.BotAI):
      
 async def on_step(self, iteration):
         self.iteration = iteration
-        await self.distribute_workers()
-        await self.build_workers()
+        await self.distribute_drones()
+        await self.build_drones()
         await self.expand()
         await self.offensive_force_buildings()
         await self.build_offensive_force()
         await self.attack()
+        
+async def build_drones(self):
+        if (len(self.units(HATCHERY)) * 16) > len(self.units(DRONE)) and len(self.units(DRONE)) < self.MAX_WORKERS:
+            for h in self.units(HATCHERY).ready.noqueue:
+               if self.can_afford(DRONE):
+                  await self.do(h.train(DRONE))
+sc2.run_game(sc2.maps.get(map_name), [
+        Bot(race, MitchyBot()),
+        Computer(enemy_race, enemy_difficulty)
+    ], realtime=realtime_condition, save_replay_as=replay_name)
